78128579b8ce4d609afe2c82944500cf4bf3a240
Mauricio Leal <mauricio_leal2010@yahoo.com.br>
2022-12-06 06:01:30 -0300

    tentativa req 5

--- src/routers/login.router.ts
+++ src/routers/login.router.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import LoginController from '../controllers/login.controller';
-import authMiddleware from '../middlewares/auth.middleware';
 
 const loginRouter = Router();
 
 const loginController = new LoginController();
 
-loginRouter.post('/', authMiddleware.validation, loginController.controllerLogin);
+loginRouter.post('/', loginController.controllerLogin);
 
 export default loginRouter;
